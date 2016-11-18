feabe79653808b43818eea56ddfcec1467f7296b
NTomasetti <nat.tomasetti@gmail.com>
2016-11-18 14:48:27 +1100

    Cleaned Layouts

--- load_forecasts/AR3MCMC.r
+++ load_forecasts/AR3MCMC.r
@@ -7,95 +7,86 @@ library(microbenchmark)
 library(ggplot2)
 library(coda)
 sourceCpp("AR3MCMC.cpp")
+sourceCpp("AR4MCMC.cpp")
 
 #Data
 
-loadjul2011 = read_csv("Vic_mrim_sum_web.csv")
-loadapr2014 = read_csv("VIC_20140401_20140630.csv")
-loadjul2014 = read_csv("VIC_20140701_20140930.csv")
-loadoct2014 = read_csv("VIC_20141001_20141231.csv")
-loadjan2015 = read_csv("VIC_20150101_20150331.csv")
-loadapr2015 = read_csv("VIC_20150401_20150630.csv")
-loadjul2015 = read.csv("VIC_20150701_20150930.csv")
-loadoct2015 = read.csv("VIC_20151001_20151231.csv")
-loadjan2016 = read_csv("VIC_20160101_20160331.csv")
-temps = read_csv("temp_86071.csv")
+loaddata = select(read_csv("fulldata.csv"), SETTLEMENTDATE, TOTALDEMAND, Date, Period, Day, Temp)
+loaddata$SETTLEMENTDATE = ymd_hms(loaddata$SETTLEMENTDATE, tz = "AEST")
 
-temps$Date = as.Date(temps$Date, origin = "1899-12-30")
-loadjul2011$DAILYT = apply(loadjul2011[,-c(1, 2, 51)], 1, sum)
+GFriday = c("2011-04-22", "2012-04-06", "2013-03-29", "2014-04-18")
+EasterS = c("2011-04-24", "2012-04-08", "2013-03-31", "2014-04-20")
+EasterM = c("2011-04-26", "2012-04-09", "2013-04-01", "2014-04-21")
+Ausday = c("2011-01-26", "2012-01-26", "2013-01-28", "2013-01-27")
+Labour = c("2011-03-14", "2012-03-12", "2013-03-11", "2014-03-10")
+Anzac = c("2011-04-25", "2012-04-25", "2013-04-25", "2014-04-25")
+Queens = c("2011-06-13", "2012-06-11", "2013-06-10", "2014-06-09")
+MelCup = c("2011-11-01", "2012-11-06", "2013-11-05", "2014-11-04")
+xmasshutdown = c("2011-12-24", "2011-12-27", "2011-12-28", "2011-12-29", 
+                 "2011-12-30", "2012-01-02", "2012-01-03", "2012-12-24", 
+                 "2012-12-27", "2012-12-28", "2012-12-29", "2012-12-30",
+                 "2013-01-02", "2013-01-03", "2013-12-23", "2013-12-24", 
+                 "2013-12-27", "2013-12-28", "2013-12-29", "2013-12-30",
+                 "2014-01-02", "2014-01-03", "2014-12-22", "2014-12-23", 
+                 "2014-12-24", "2014-12-27", "2014-12-28", "2014-12-29", 
+                 "2014-12-30")
 
-allloads = rbind(loadjul2011, loadapr2014, loadjul2014, loadoct2014, loadjan2015, loadapr2015, loadjul2015, loadoct2015, loadjan2016)
-allloads$SETTD = as.Date(allloads$SETTD, format = "%d/%m/%Y")
-loadlong = select(allloads, -DCTC, - DAILYT) %>% gather(hour, load, -c(PROFILEAREA, SETTD))
-loadlong = mutate(loadlong, hour = as.numeric(substr(hour, 4, 5)))
-loadlong = summarise(group_by(loadlong, hour, SETTD), load = sum(load))
+loaddata = mutate(loaddata, Christmas = ifelse(substr(Date, 6, 10) == "12-25", 1, 0), Boxingday = ifelse(substr(Date, 6, 10) == "12-26", 1, 0),
+                  NYE = ifelse(substr(Date, 6, 10) == "12-31", 1, 0), NYD = ifelse(substr(Date, 6, 10) == "01-01", 1, 0),
+                  Easter = ifelse(substr(Date, 1, 10) %in% EasterS, 1, 0), GoodFriday = ifelse(substr(Date, 1, 10)%in% GFriday, 1, 0), 
+                  OtherHoliday = ifelse(substr(Date, 1, 10) %in% c(EasterM, Ausday, Labour, Anzac, Queens, MelCup, xmasshutdown), 1, 0))
 
-loadsubset = filter(loadlong, SETTD < "2015-03-01")
-loadsubset = plyr::arrange(loadsubset, SETTD, hour)
-tempsubset = filter(temps, Date > "2011-06-30")
+loaddata = mutate(loaddata, AbsTemp = abs(Temp - 20))
 
-alldata = data.frame(tempsubset, load = loadsubset$load)
-alldata = mutate(alldata, day = factor(wday(Date)), Period = factor(Period), AbsTemp = abs(Temp - 18.3))
-alldata$trend = 1:dim(alldata)[1]
 
-X = model.matrix(load ~ Period + day + AbsTemp, data = alldata)
-Y = alldata$load
+X = model.matrix(TOTALDEMAND ~ Day + Christmas + Boxingday + NYE + NYD + Easter + GoodFriday + OtherHoliday + AbsTemp, data = loaddata)
+Y = loaddata$TOTALDEMAND
 
 #MCMC Algorithm
 
-rep = 50000
-dimx = ncol(X)
-T = length(Y)
-
-#Beta - 0 intercept,  1-47 half hour effects, 48-53 day of week, 54 temp, dimx linear time trend, base - sunday 12:00
-mu = rep(0, dimx)
-tau = rep(10000^2, dimx)
-gamma = rep(0, 3)
-lambda = rep(10, 3)
-alphasig = 1
-betasig = 1
-alphabar = alphasig + T/2 - 168
-
-#calculations
-sumxx = xx(X, T, dimx)
-sumyx = yx(Y, X, T, dimx)
-sumyy = yy(Y, T)
-
-#initial states
-beta = rep(0, dimx)
-phi = rep(0, 3)
-sigmasq = 1
-
-draws = matrix(0, nrow = rep, ncol = dimx + 4)
-
-for(i in 1:(rep/1000)){
-  draws[((i-1)*1000+1):(i*1000) , ] = MCMC_looponly(1000, Y, X, T, dimx, sumyy, sumyx, sumxx, beta, phi, sigmasq, mu, tau, lambda, gamma, alphabar, betasig)
-  beta = draws[i*1000, 1:dimx]
-  phi = draws[i*1000, (dimx + 1) : (dimx + 3)]
-  sigmasq = draws[i * 1000, (dimx + 4)]
-}
-
-
-#MCMC = MCMC_load(Y, X, rep)
+MCMC = MCMC_load(Y, X, 25000)
+#MCMC = MCMC_load4(Y, X, 5000)
 
 #Diagnostics
-effectiveSize(MCMC[(rep/5+1):rep, ])
-posmean = apply(MCMC[(rep/5+1):rep, ], 2, mean)
+posterior.statistics = function(x){
+  l95 = quantile(x, probs = 0.025)
+  mean = mean(x)
+  u95 = quantile(x, probs = 0.975)
+  return(c(l95, mean, u95))
+}
 
-xbetahat = colSums(t(X) * posmean[1:55])
+keep = seq(5001, 25000, 40)
+MCMCdf = as.data.frame(MCMC[keep,])
+colnames(MCMCdf) = c(colnames(X), "Phi1", "Phi48", "Phi336", "Sigmasq")
+MCMCdf$iter = keep
+MCMC.l = gather(MCMCdf, parameter, draw, -iter)
+ggplot(MCMC.l, aes(iter, draw)) + geom_line() + facet_wrap(~parameter, scales = "free") + theme(axis.ticks.x = element_blank(), 
+                                                                                                axis.text.x = element_blank(),
+                                                                                                axis.ticks.y = element_blank(),
+                                                                                                axis.text.y = element_blank())
+
+effectiveSize(MCMCdf)
+posterior = apply(MCMCdf[,1:19], 2, posterior.statistics)
+row.names(posterior) = c("Lower 95", "Mean", "Upper 95")
+posterior
+posmean = posterior[2,]
+xbetahat = colSums(t(X) * posmean[1:15])
+demeaned = Y - xbetahat
+qplot(loaddata$SETTLEMENTDATE, demeaned, geom = "line")
+
 yhat = vector(length = length(Y))
 yhat[1:336] = Y[1:336]
 for(i in 337:length(Y)){
-  yhat[i] = xbetahat[i] + posmean[56]*(yhat[i-1] - xbetahat[i-1]) + posmean[57]*(yhat[i-48] - xbetahat[i-48]) +
-    posmean[58]*(yhat[i-336] - xbetahat[i-336]) 
+  yhat[i] = xbetahat[i] + posmean[16]*(yhat[i-1] - xbetahat[i-1]) + posmean[17]*(yhat[i-48] - xbetahat[i-48]) + posmean[18]*(yhat[i-336] - xbetahat[i-336]) 
 }
+qplot(loaddata$SETTLEMENTDATE, yhat, geom = "line")
+
+
 ypred = vector(length = length(Y))
 ypred[1:336] = Y[1:336]
 for(i in 337:length(Y)){
-  ypred[i] = xbetahat[i] + posmean[57]*(Y[i-1] - xbetahat[i-1]) + posmean[58]*(Y[i-48] - xbetahat[i-48]) +
-    posmean[59]*(Y[i-336] - xbetahat[i-336]) 
+  ypred[i] = xbetahat[i] + posmean[16]*(Y[i-1] - xbetahat[i-1]) + posmean[17]*(Y[i-48] - xbetahat[i-48]) + posmean[18]*(Y[i-336] - xbetahat[i-336]) 
 }
-
-plot(alldata$Date, Y-yhat)
-
+qplot(loaddata$SETTLEMENTDATE, Y-ypred, geom = "line")
+mean(abs(Y-ypred))
 
