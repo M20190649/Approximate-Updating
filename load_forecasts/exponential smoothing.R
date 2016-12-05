63c6bdd3be42ab02633d0023eaacc472ac84f5f7
NTomasetti <nat.tomasetti@gmail.com>
2016-12-05 17:08:25 +1100

    Reparameterised seasonal effects to solve singular matrix

---
+++ load_forecasts/exponential smoothing.R
@@ -0,0 +1,86 @@
+library(ggplot2)
+sim_exp_smoothing = function(params, lags, initial, T, sigmasq){
+  m = length(params)
+  dim = sum(lags) - m + 1
+  if(length(lags) != m){
+    stop("Parameter and lag vectors must be the same length")
+  }
+  if(length(initial) != dim){
+    stop("Length of initial state vector must equal sum of lag vector minus number of latent states plus one")
+  }
+  if(any(params >= 1 | params <= 0)){
+    stop("All parameters must be between zero and one")
+  }
+  states = matrix(0, ncol = T+1, nrow = dim)
+  states[,1] = initial
+  y = vector(length = T+1)
+  newstates = c(1, 1+cumsum(lags))[-(m+1)]
+  for(t in 2:(T+1)){
+    y[t] = rnorm(1, 0, sqrt(sigmasq)) + states[1, t-1] - sum(states[(1:sum(lags))[-cumsum(lags)],t-1])
+    states[1, t] = (1-params[1])*states[1, t-1] + params[1]*(y[t] + sum(states[(1:sum(lags))[-cumsum(lags)],t-1])) 
+    
+    
+    for(j in 2:m){
+      states[newstates[j], t] = -sum(states[(cumsum(lags)[j-1]+1):(cumsum(lags)[j]-1),t-1]) +
+                                       params[j]*(y[t] - states[1, t-1] + sum(states[(1:sum(lags))[-cumsum(lags)], t-1]))
+    }
+    for(j in (1:(sum(lags)-m+1))[-newstates]){
+      states[j, t] = states[j-1, t-1]
+    }
+  }
+  out = list(y = y[2:(T+1)], states = states[newstates,2:(T+1)])
+  return(out)
+}
+
+alpha = c(0.3, 0.5)
+lags = c(1, 7)
+initial = c(0, 1, 2, 6, 1, 1, -3)
+T = 10000
+sigmasq = 1
+exp_data = sim_exp_smoothing(alpha, lags, initial, T, sigmasq)
+ggplot() + geom_line(aes(1:T, exp_data$y), colour = "red") + 
+  geom_line(aes(1:T, exp_data$states[1,]), colour = "black") + geom_line(aes(1:T, exp_data$states[2, ]), colour = "blue")
+
+##MCMC Setup
+m = length(lags)
+dim = sum(lags) - m +1
+y = exp_data$y
+x = c(1, rep(-1, dim-1))
+Trans = matrix(0, dim, dim)
+Trans[1,1] = 1
+newstates = c(1, 2+cumsum(lags-1)[-m])
+for(i in 2:m){
+  if(i < m){
+    Trans[newstates[i], newstates[i]:(newstates[i+1]-1)] = -1
+  } else {
+    Trans[newstates[m], newstates[m]:dim] = -1
+  }
+}
+for(i in (1:dim)[-newstates]){
+  Trans[i, i-1] = 1
+}
+
+
+rep = 10000
+
+grid = expand.grid(seq(0.1, 0.9, 0.1), seq(0.1, 0.9, 0.1))
+grid$dens = apply(grid, 1, alpha_xt, y = y, Trans = Trans, x = x, lags = lags, initial = initial, sigmasq = sigmasq)
+ggplot(grid) + geom_tile(aes(Var1, Var2, fill = dens))
+grid[which.max(grid$dens),]
+
+
+
+#Progress:
+#Fix problem in eigenvalue of D matrix (coding error)
+#Write simulation and MCMC code (in progress - alpha draws)
+#Discover problem with XTX being singular
+#Reparameterise seasonal effects to have equivalent to XTX that is not singluar
+#Fix old code to work with new parameterisation
+
+#To do:
+#Figure out best way to draw alpha from marginal (probably add extra version of calculation code that doesn't have the structure - just return density)
+#Run and see what happens
+#Hope to fit marginals to easy to use densities
+#Figure out copula structure and fit
+#Write Variational Bayes algorithm (and hope distribution allows reparameterisation)
+
--- load_forecasts/exponential_smoothing.cpp
+++ load_forecasts/exponential_smoothing.cpp
@@ -7,152 +7,118 @@ using namespace Rcpp;
 using namespace arma;
 using namespace std;
 
-mat statevector(vec y, vec alpha, vec initial, int T){
-  mat states(385, T+1);
-  states.row(0) = initial;
-  for(int t = 1; t < T+1; ++t){
-    states(0, t) = (1 - alpha[0])*states(0, t-1) - alpha[0]*states(48,t-1) - alpha[0]*states(384, t-1) + alpha[0]*y[t];
-    states(1, t) = (1 - alpha[1])*states(48, t-1) - alpha[1]*states(0,t-1) - alpha[1]*states(384, t-1) + alpha[1]*y[t];
-    states(49, t) = (1 - alpha[2])*states(384, t-1) - alpha[2]*states(0,t-1) - alpha[2]*states(48, t-1) + alpha[2]*y[t];
-    for(int j = 2; j < 385; ++j){
-      if(j != 49) {
-        states(j, t) = states(j-1, t-1);
-      }
-    }
-  }
-  return states;
-}
+struct alpha_details {
+  double density; //marginal density
+  mat xtxinv; //x transpose x inverse
+  double ssquared; //data variance
+  vec beta0hat; //OLS estimator 
+};
 
-// [[Rcpp::export]]
-mat m_alpha(vec alpha, vec y){
-  int T = y.n_elem;
-  mat D(385, 385);
-  sp_mat Trans(385, 385);
-  Trans(0, 0) = Trans(1, 48) = Trans(49, 384) = 1;
-  for(int i = 2; i < 385; ++i){
-    if(i != 49) {
-      Trans(i, i-1) = 1;
+alpha_details alpha_marginal(vec y, vec alpha, mat Trans, vec x, vec lags){
+  int size = lags.n_elem; //Number of parameters in alpha
+  int T = y.n_elem; //Length of time series
+  int dim = sum(lags) - size + 1; //Sum of lags is dimension of Trans/D/states
+  mat x_tilde(T, dim, fill::zeros); //X_tilde_t vector for t = 1.. T
+  vec y_tilde(T);  //Y_tilde_t scalar for t = 1...T
+  mat b_bar(dim, T ); //b predictions
+  vec alpha_full(dim, fill::zeros); //entire parameter vector including zeroes for b_t = T b_t-1 + alpha_full*et
+  vec csum_lags(size, fill::zeros); //cumulative sum of lags, starting at 0, omitting last term
+  //cumsum is to indicate which elements of alpha_full are nonzero, should have indices for lt and first value of each seasonal effect
+  //In comparision x typically had lt and last element of each seasonal effect (in old parameterisation)
+  
+  //Create D matrix
+  for(int i = 0; i < size; ++i){
+    if(i > 0) { //first term is already set to 0
+      csum_lags[i] = csum_lags[i-1] + lags[i-1]; 
     }
+    alpha_full[csum_lags[i]] = alpha[i]; //nonzero element of alpha_full at cumsum values (-1 for count starting at 0)
   }
-  mat x_tilde(T, 385);
-  vec y_tilde(T);
-  mat b_bar(385, T);
-  mat xtxinv(385, 385);
-  vec x(385, fill::zeros);
-  x[0] = x[48] = x[384] = 1;
-  vec alpha_full(385, fill::zeros);
-  alpha_full[0] = alpha[0];
-  alpha_full[1] = alpha[1];
-  alpha_full[49] = alpha[2];
-  D = Trans - x * alpha_full.t();
-  return D;
-  //vec s_squared(1);
-  //vec s(T);
-  //vec beta_0_hat(385);
+  mat D(dim, dim); //D matrix
+  D = Trans - alpha_full * x.t();
   
   //Calculate tilde data
-  //x_tilde.row(0) = x.t() * D;
-  //b_bar.col(0) = alpha_full * y[0];
-  //y_tilde[0] = y[0];
-  //for(int t = 1; t < T; ++t){
-  //  b_bar.col(t) = D * b_bar.col(t-1) + alpha_full * y[t];
-  //  x_tilde.row(t) = x_tilde.row(t-1) * D; 
-  //  y_tilde[t] = (y[t] - x.t() * b_bar.col(t-1))[0]; 
-  //}
-
-  //xtxinv = inv(x_tilde.t() * x_tilde);
-  //beta_0_hat = xtxinv * x_tilde.t() * y_tilde;
-  //s = y_tilde - x_tilde * beta_0_hat;
-  //s_squared = s.t() * s;
+  x_tilde.row(0) = x.t(); //x_tilde_1
+  b_bar.col(0) = alpha_full * y[0]; //b_bar_1
+  y_tilde[0] = y[0]; //y_tilde_1
+  for(int t = 1; t < T; ++t){ //Loop for other values, recursion in paper
+    b_bar.col(t) = D * b_bar.col(t-1) + alpha_full * y[t];
+    x_tilde.row(t) = x_tilde.row(t-1) * D; 
+    y_tilde[t] = (y[t] - x.t() * b_bar.col(t-1))[0]; 
+  }
   
-  //double density = (pow(det(xtxinv), 0.5) * pow(s_squared/(T-3), (T-3)/2))[0];
-  //return density;
+  //Final computations
+  mat xtxinv(dim, dim);
+  xtxinv = (x_tilde.t() * x_tilde).i();
+  vec beta0hat(dim);
+  beta0hat = xtxinv * x_tilde.t() * y;
+  double ssquared;
+  ssquared = ((y_tilde - x_tilde * beta0hat).t() * (y_tilde - x_tilde * beta0hat) / (T - size))[0];
+  double density;
+  density - pow(det(xtxinv), 0.5) * ssquared * (-(T - size)/2);
+  
+  //Returning results
+  alpha_details results;
+  results.density = density;
+  results.xtxinv = xtxinv;
+  results.ssquared = ssquared;
+  results.beta0hat = beta0hat;
+  return results;
 }
-
 
 
 // [[Rcpp::export]]
-mat exponential_smoothing(vec y, int rep){
-  //Setup
-  int T = y.n_elem;
-  vec x(385, fill::zeros);
-  x[0] = x[48] = x[384] = 1;
-  sp_mat Trans(385, 385);
-  Trans(0, 0) = Trans(1, 48) = Trans(49, 384) = 1;
-  for(int i = 2; i < 49; ++i){
-    Trans(i, i-1) = 1;
-  }
-  for(int i = 50; i < 385; ++i){
-    Trans(i, i-1) = 1;
-  }
-
-  sp_mat D(385, 385);
-  vec alpha(3);
-  mat draws(rep, 7);
-  mat x_tilde(T, 385);
-  vec y_tilde(T);
-  mat b_bar(385, T);
-  mat xtxinv(385, 385);
-  double c = 0; //replace with max m(alpha)
+mat general_exp_sm_MCMC(vec y, int rep, vec x, mat Trans, vec lags){
+  //Inputs are data, MCMC draws, x vector, T matrix, and lag structure, eg. lags = (1, 48, 336) for level, daily and weekly seasonals
+  int T = y.n_elem; //Length of time series
+  int size = lags.n_elem; //Number of seasonal effects plus one for level
+  int dim = sum(lags) - size + 1; //Parameterisation includes 1 term for l, then max lag - 1 for each seasonal effect.
+  mat draws(rep, dim + size + 1); //size many smoothing parameters, dim many initial states, plus sigma squared
+  //First columns are for smoothing parameters, then initial states, then sigma squared
   
-  double IG_shape = (T - 385)/2;
+  //Initialising various parameters to be drawn
+  vec initial(dim); //initial states of latent variables
+  double sigmasq; //variance
+  vec alpha(size); //smoothing parameters
+  
+  //Used in density calculations
+  alpha_details alpha_result; //Marginal density result plus all the data calculations required stored in a structure
+  double IG_shape = (T - dim)/2;
   double IG_scale;
-  vec s_squared(1);
-  vec s(T);
-  vec beta_0_hat(385);
+  mat var(dim, dim); //Initial states have MVN conditional, store the variance here
+  mat L(dim, dim); //For the lower triangle of var
+  vec eps(dim); //For standard normal variables in the MVN
   
-  //main loop
+  //Main loop
   for(int r = 0; r < rep; ++r){
-    
-    //Draw alpha candidate from uniform, apply accept-reject sampling
-    bool flag = FALSE;
-    while(!flag){
-      alpha = randu<vec>(3);
-      vec alpha_full(385, fill::zeros);
-      alpha_full[0] = alpha[0];
-      alpha_full[1] = alpha[1];
-      alpha_full[49] = alpha[2];
-      D = Trans - x * alpha_full.t();
-      
-      //Calculate tilde data
-      x_tilde.row(0) = x.t() * D;
-      b_bar.col(0) = alpha_full * y[0];
-      y_tilde[0] = y[0];
-      for(int t = 1; t < T; ++t){
-        b_bar.col(t) = D * b_bar.col(t-1) + alpha_full * y[t];
-        x_tilde.row(t) = x_tilde.row(t-1) * D; 
-        y_tilde[t] = (y[t] - x.t() * b_bar.col(t-1))[0]; 
-      }
-      
-      xtxinv = inv(x_tilde.t() * x_tilde);
-      beta_0_hat = xtxinv * x_tilde.t() * y_tilde;
-      s = y_tilde - x_tilde * beta_0_hat;
-      s_squared = s.t() * s;
-      
-      double acceptance_ratio = (pow(det(xtxinv), 0.5) * pow(s_squared/(T-385), (T-385)/2))[0]/c;
-      double u = randu<vec>(1)[0];
-      if(u < acceptance_ratio){
-        flag = TRUE;
-      }
-    }
-    draws(r, 0) = alpha[0];
-    draws(r, 1) = alpha[1];
-    draws(r, 2) = alpha[2];
 
-    //Draw sigma squared from IG
-    IG_scale = 2/(s_squared[0]);
+    //Draw from alpha_marginal somehow.
+    //Will probably have to calculate density over a grid and either find CDF or accept/reject?
+    alpha_result = alpha_marginal(y, alpha, Trans, x, lags);
     
-    draws(r, 3) = randg<vec>(1, distr_param(IG_shape, 1/IG_scale))[0];
+    //Sigmasquared from Inverse Gamma Density
+    IG_scale = (T - size) / 2 * alpha_result.ssquared;
+    sigmasq = 1 / randg<vec>(1, distr_param(IG_shape, 1/IG_scale))[0]; //Arma uses gamma(shape scale), convert IG scale to rate
     
-    //Draw beta_0 from MVN
-    mat L = chol(draws(r, 3) * xtxinv, "lower");
-    vec eps = randn<vec>(3);
-    draws(r, 4) = beta_0_hat[0] + L(0, 0) * eps[0];
-    draws(r, 5) = beta_0_hat[1] + L(1, 0) * eps[0] + L(1, 1) * eps[1];
-    draws(r, 6) = beta_0_hat[49];
-    for(int i = 0; i < 50; ++i){
-      draws(r, 6) += L(49, i) * eps[i];
+    //Initial states from Gaussian Density, MVN via cholesky decomposition
+    var = sigmasq * alpha_result.xtxinv;
+    L = chol(var, "lower");
+    eps = randn<vec>(dim); //Standard normal variables
+    for(int i = 0; i < dim; ++i){
+      initial[i] = alpha_result.beta0hat[i]; //Add means to MVN
+      for(int j = 0; j <= i; ++j){
+        initial[i] += L(i, j) * eps[j]; //Add sum of lower triangle + epsilon
+      }
     }
-  } 
+    
+    //Storage
+    for(int i = 0; i < size; ++i){ 
+      draws(r, i) = alpha[i];
+    }
+    draws(r, size) = sigmasq;
+    for(int i = 0; i < dim; ++i){
+      draws(r, size + 1 + i) = initial[i];
+    }
+  }
   return draws;
 }
+
