03db12335eb88f05760f892b435ff38c3fc66b02
NTomasetti <nltom2@student.monash.edu.au>
2016-12-14 21:34:43 +1100

    Work on SGA algorithm

--- load_forecasts/exp_sm_SGA.R
+++ load_forecasts/exp_sm_SGA.R
@@ -3,11 +3,13 @@ library(readr)
 library(pscl)
 
 ##SGA Algorithm
-#Inverse CDF function is easier in R
+
+#R only functions
 invert_cdf = function(u, lambda, i){
   if(i == 1 | i == 2){
     support = seq(0, 1, 0.001)
-    dens = lambda[5] * dnorm(support, lambda[1], lambda[2]) + (1-lambda[5]) * dnorm(support, lambda[3], lambda[4])
+    dens = lambda[5*i] * dnorm(support, lambda[5*(i-1) + 1], lambda[5*(i-1) + 2]) + 
+      (1-lambda[5*i]) * dnorm(support, lambda[5*(i-1) + 3], lambda[5*(i-1) + 4])
     CDF = cumsum(dens)
     u = u * max(CDF)
     draws = rep(0, length(u))
@@ -15,15 +17,27 @@ invert_cdf = function(u, lambda, i){
       draws[i] = support[min(which(CDF > u))]
     }
   } else if(i == 3) {
-    draws = qigamma(u, lambda[1], lambda[2])
+    draws = qigamma(u, lambda[11], lambda[12])
   } else {
-    draws = qnorm(u, lambda[1], lambda[2])
+    draws = qnorm(u, lambda[5+2*i], lambda[6+2*i])
   }
   return(draws)
 }
 
+simtheta= function(s, RVM, lambda){
+  #Simulate theta - R only
+  #Require RVM Object created from Vine Structure (prespecified) and dependence parameters (eta)
+  simvine = RVineSim(s, RVM)
+  #Require Inverse CDF transformation for all marginals from mean field parameters (lambda)
+  sim = matrix(0, s, 10)
+  for(i in 1:10){
+    sim[,i] = invert_cdf(simvine[,s], lambda[[s]], i)
+  }
+  return(sim)
+}
+
 #Setup:
-#y, x, Trans, lags, VB Initialisation, Vine object loaded
+#SGA parameters, reading data, initialisation of lambda, eta
 s = 100 #samples per iteration
 k = 5 #minimum number of iterations inside loop
 K = 2 #minimum number of iterations outside loop
@@ -33,78 +47,81 @@ threshold = 0.001 #convergence threshold
 steptuning = 0.1 #for adagrad algorithm, can be vectorised
 
 y = read_csv("simy.csv")
-lags = c(1, 7)
-m = length(lags)
-dim = sum(lags) - m +1
-x = c(1, rep(-1, dim-1))
-Trans = matrix(0, dim, dim)
-Trans[1,1] = 1
-newstates = c(1, 2+cumsum(lags-1)[-m])
-for(i in 2:m){
-  if(i < m){
-    Trans[newstates[i], newstates[i]:(newstates[i+1]-1)] = -1
-  } else {
-    Trans[newstates[m], newstates[m]:dim] = -1
-  }
-}
-for(i in (1:dim)[-newstates]){
-  Trans[i, i-1] = 1
-}
 Vine = readRDS("Vine.rds")
 
-lambda = list()
-eta = list()
+lambdalist = list(alpha = c(),
+              delta = c(),
+              sigmasq = c(),
+              il = c(),
+              is1 = c(),
+              is2 = c(),
+              is3 = c(),
+              is4 = c(),
+              is5 = c(),
+              is5 = c()) #use list for easy initialisation
+lambda = unlist(lambda)
+eta = list(eta1 = Vine$par,
+           eta2 = Vine$par2) #keep in same format as the RVM object
+cop_structure = matrix(0, 45, 2)
 repso = 0
-LBo = ELBO(lambda, eta)
+RVM = RVineMatrix(Matrix, family, eta[[1]], eta[[2]])
+theta = simtheta(10, RVM, lambda)
+LBo = ELBO(y, lambda, eta[[1]], eta[[2]], theta, x, Trans, dim, T, lags, 10)
 diffo = 1
 #Main Loop
 while(diffo > threshold | repso < K) {
   repso = reps + 1
-  ##Step One: Simulate theta - R only
-  #Require RVM Object created from Vine Structure (prespecified) and dependence parameters (eta)
-  #Matrix given by Vine object
-  #Family given by Vine object
-  #params given by eta
-  RVM = RVineMatrix(Matrix, family, par, par2)
-  simvine = RVineSim(s, RVM)
-  #Require Inverse CDF transformation for all marginals from mean field parameters (lambda)
-  sim = matrix(0, s, 10)
-  for(i in 1:10){
-    sim[,i] = invert_cdf(simvine[,s], lambda[[s]], i)
+
+  
+  ##Step One - subloop: Maximise over lambda - can probably be evaluated in cpp (unsure on copulas)
+  #Simulate Theta
+  #Evalute log joint density and log q density from last lambda, eta
+  #Evaluate derivative of log q density wrt lambda
+  #Calculate change in lambda
+  #Check ELBO Convergence
+  diffi = 1
+  RVM = RVineMatrix(Matrix, family, eta[[1]], eta[[2]])
+  repsi = 0
+  LBi = LB
+  Gt = rep(0, 26)
+  while(diffi > threshold | repsi < k){
+    if(repsi > M){
+      break
+    }
+    repsi = repsi + 1
+    theta = simtheta(s, RVM, lambda)
+    update = SGA_lambda(y, theta, lambda, eta[[1]], eta[[2]], steptuning, Gt, cop_structure, Vine$type, s)
+    lambda = update[1,]
+    Gt = update[2,]
+    LBnew = ELBO(y, lambda, eta[[1]], eta[[2]], theta, x, Trans, dim, T, lags, 10)
+    diffi = abs(LBnew - LBi)
+    Lbi = LBnew
   }
   
-##Step Two - subloop: Maximise over lambda - can probably be evaluated in cpp (unsure on copulas)
-  diffi = 1
-  LBi = LBo
-  repsi = 0
-  while(diffi > threshold | repsi < k) {
-    repsi = repsi + 1
-    lambdanew = SGA_lambda()
-    LBnew = ELBO(lambdanew, eta)
-    diffi = abs(LBnew - LBi)
-    LBi = LBnew
-    lambda = lambdanew
-  }
-#Evalute log joint density and log q density from last lambda, eta
-#Evaluate derivative of log q density wrt lambda
-#Calculate change in lambda
-#Check ELBO Convergence
-
-##Step Three: Maximise over eta
+  ##Step Two: Maximise over eta
+  #Essentially the same as Step One, need to rebuild RVM object after each change in eta
   diffi = 1
   repsi = 0
+  Gt = array(0, dim = c(10, 10, 2))
   while(diffi > threshold | repsi < k) {
+    if(repsi > M){
+      break
+    }
     repsi = repsi + 1
-    etanew = SGA_eta(lambda, etanew)
-    LBnew = ELBO(lambdanew)
+    RVM = RVineMatrix(Matrix, family, eta[[1]], eta[[2]])
+    theta = simtheta(s, RVM, lambda)
+    update = SGA_eta1(y, theta, lambda, eta[[1]], eta[[2]], steptuning, Gt[,,1], cop_structure, Vine$Type, s)
+    eta[[1]] = update[,,1]
+    Gt[,,1] = update[,,2]
+    update = SGA_eta2(y, theta, lambda, eta[[1]], eta[[2]], steptuning, Gt[,,2], cop_structure, Vine$Type, s)
+    eta[[2]] = update[,,1]
+    Gt[,,2] = update[,,2]
+    LBnew = ELBO(y, lambda, eta[[1]], eta[[2]], theta, x, Trans, dim, T, lags, 10)
     diffi = abs(LBnew - LBi)
     LBi = LBnew
-    eta = etanew
   }
-#Essentially the same as Step Two
 
-#Step Four:
-#Check ELBO Convergence, decide to repeat loop or not
+  ##Step Three: Check ELBO Convergence, decide to repeat loop or not
   diff = abs(LBi - LBo)
   LBo = LBi
 }
--- load_forecasts/exp_sm_SGA.cpp
+++ load_forecasts/exp_sm_SGA.cpp
@@ -6,27 +6,29 @@
 using namespace Rcpp;
 using namespace arma;
 using namespace std;
+#define my_PI 3.1415926535897932384626433832795028841971693993751058209749445923078164062
 
-//Calculate ELBO to check convergence
-// [[Rcpp::export]]
-double ELBO(vec lambda, vec y, int reps = 10){}
-
-//Calculate partial derivatives
-double deriv(vec theta, vec lold, vec y, int j, mat Trans, int dim, int T, vec lags, int m){}
 
 //Evaluate log joint density
-double log_joint_dens(vec y, vec alpha, double sigmasq, vec initial, vec x, mat Trans, int dim, int T, vec lags){
+double log_joint_dens(vec y, vec theta){
+  int T = y.n_elem;
+  double sigmasq = theta[2];
+  vec initial = {theta[3], theta[4], theta[5], theta[6], theta[7], theta[8], theta[9]};
+  int dim = 7;
+  vec lags = {1, 7};
+  vec alpha_full = {theta[0], theta[1], 0, 0, 0, 0, 0};
+  vec x = {1, -1, -1, -1, -1, -1, -1};
+  mat Trans(7, 7, fill::zeros);
+  Trans(0, 0) = 1;
+  Trans.row(1) = {0, -1, -1, -1, -1, -1, -1};
+  for(int i = 2; i < 7; ++i){
+    Trans(i, i-1) = 1;
+  }
   mat x_tilde(T, dim, fill::zeros); //X_tilde_t vector for t = 1.. T
   vec y_tilde(T);  //Y_tilde_t scalar for t = 1...T
   mat b_bar(dim, T ); //b predictions
   vec alpha_full(dim, fill::zeros); //entire parameter vector including zeroes for b_t = T b_t-1 + alpha_full*et
   vec csum_lags(size, fill::zeros); //cumulative sum of lags, starting at 0, omitting last term
-  for(int i = 0; i < size; ++i){
-    if(i > 0) { //first term is already set to 0
-      csum_lags[i] = csum_lags[i-1] + lags[i-1]; 
-    }
-    alpha_full[csum_lags[i]] = alpha[i]; //nonzero element of alpha_full at cumsum values (-1 for count starting at 0)
-  }
   mat D(dim, dim); //D matrix
   D = Trans - alpha_full * x.t();
   x_tilde.row(0) = x.t(); //x_tilde_1
@@ -44,44 +46,125 @@ double log_joint_dens(vec y, vec alpha, double sigmasq, vec initial, vec x, mat 
   return density;
 }
 
-vec SGA_exp_smoothing(vec y, vec initial, int s, double eta, double threshold, int M, vec lags, mat Trans, vec x){
-  int n = initial.n_elem; //Number of parameters to optimise over
-  int m = lags.n_elem;
-  int dim = sum(lags) - m + 1;
-  vec lnew = initial; //Update paramaeters
-  vec lold(n); //Store current parameters
-  vec partials(n); //Store partial derivatives in calculation 
-  vec Gt = zeros<vec>(n); //Step size intermediate calculation
+double densigammac(double x, double alpha, double beta){
+  double cons = pow(beta, alpha) / tgamma(alpha); //constant
+  double kernel = pow(x, -(alpha + 1)) * exp(-beta/x); //kernel of distribution
+  return cons * kernel;
+}
+
+double dnormc(double x, double mu, double sigmasq){
+  double cons = 1 / sqrt(2*my_PI*sigmasq);
+  double kernel = exp( - 1 / (2 * sigmasq) * pow(x - mu, 2));
+  return cons * kernel;
+}
+
+double logq(vec theta, vec lambda, mat par1, mat par2, mat copula, mat type){
+  vec marginals(10);
+  vec copulas(45); //Remove elements for each independence copula
+  int type;
+  for(int i = 0; i < 10; ++i){
+    if(i == 0 | i == 1){
+      marginals[i] = log(lambda[5*i + 4] * dnormc(theta[i], lambda[5*i], lambda[5*i + 1]) + 
+        (1-lambda[5*i] + 4) * dnormc(theta[i], lambda[5*i + 2], lambda[5*i + 3]));
+    } else if (i == 2) {
+      marginals[i] = log(densigammac(theta[i], lambda[10], lambda[11]));
+    } else {
+      marginals[i] = log(dnormc(theta[i], lambda[6 + 2*i], lambda[7 + 2*i]));
+    }
+  }
+  for(int i = 0; i < 45; ++i){
+    type = type(copula(i, 0), copula(i, 1));
+    if(type == 1) {
+      
+    } else if(type == 2) {
+      
+    } //Continue for all types of copula
+    //May need to code copula density functions themselves
+  }
+  return sum(marginals) + sum(copulas);
+}
+
+double deriv_lambda(vec theta, vec lambda, mat par1, mat par2, vec y, mat copula, mat type, int j){}
+
+double deriv_eta1(vec theta, vec lambda, mat par1, mat par2, vec y, mat copula, mat type, int j){}
+
+double deriv_eta2(vec theta, vec lambda, mat par1, mat par2, vec y, mat copula, mat type, int j){}
+
+//Calculate ELBO to check convergence
+// [[Rcpp::export]]
+double ELBO(vec y, vec lambda, mat par1, mat par2, mat theta, int reps = 10){
+  double output = 0;
+  for(int i = 0; i < reps; ++i){
+    output += (log_joint_dens(y, theta.row(i)) - logq(theta.row(i), lambda, par1, par2))/reps;
+  }
+  return output;
+}
+
+// [[Rcpp::export]]
+mat SGA_lambda(vec y, vec theta, vec lambda, mat par1, mat par2, double tuning, vec Gt, mat copula, mat type, int s){
+  int n = lambda.n_elem;
+  vec partials(n); partials.fill(0); //Store partial derivatives in calculation 
   vec pt(n); //Step size
-  double k = 0; //Number of iterations
-  double LBold; //Current lower bound
-  double LBnew = ELBO(lnew, 10, y); //Updated lower bound
-  double diff; //Difference between the two
-  mat theta(s, 10); //Simulated variables
-  
-  while(diff > threshold | k < 50){
-    if(k > M){ //Ensure algorithm stops if it fails to converge
-      // Print some failure message to console
-      break; 
+  for(int i = 0; i < s; i++){ //for each sample in s
+    for(int j = 0; j < n; j++){  //and for each parameter in lambda
+      partials[j] += deriv_lambda(theta.row(i), lambda, par1, par2, y, copula, type, j)/s; //partial deriv = mean of s monte carlo estimates
     }
-    lold = lnew; //Push last iteration parameters to 'old'
-    LBold = LBnew;
-    partials.fill(0); //Sum for partial diffs starts at 0
-    theta = simulate(s, lnew); //Write this simulation function
-    for(int i = 0; i < s; i++){ //for each sample in s
-      for(int j = 0; j < n; j++){  //and for each parameter in lambda
-        partials[j] += deriv(theta.row(i), lold, y, j)/s; //partial deriv = mean of s monte carlo estimates
-      }
+  }
+  for(int j = 0; j < n; j++){
+    Gt[j] += pow(partials[j],2); //Each GT is the sum of the squared partial diffs. 
+    pt[j] = tuning * pow(Gt[j], -0.5); //transform to get pt value
+    lambda[j] = lambda[j] + pt[j]*partials[j];  //new lambda is old + stepsize * partial diff
+  }
+  mat output(2, n);
+  output.row(0) = lambda;
+  output.row(1) = Gt;
+  return output;
+}
+
+// [[Rcpp::export]]
+cube SGA_eta1(vec y, vec theta, vec lambda, mat par1, mat par2, double tuning, mat Gt, mat copula, mat type, int s){
+  int n = copula.n_rows;
+  mat partials(10, 10, fill::zeros); //Store partial derivatives in calculation 
+  mat pt(10, 10, fill::zeros); //Step size
+  for(int i = 0; i < s; i++){ //for each sample in s
+    for(int j = 0; j < n; ++j){ //and for each parameter in eta
+        partials(copula(j, 0), copula(j, 1)) += 
+          deriv_eta1(theta.row(i), lambda, par1, par2, y, copula, type, j)/s;
+          //partial deriv = mean of s monte carlo estimates
     }
-    for(int j = 0; j < n; j++){
-      Gt[j] += pow(partials[j],2); //Each GT is the sum of the squared partial diffs. 
-      pt[j] = eta * pow(Gt[j], -0.5); //transform to get pt value
-      lnew[j] = lold[j] + pt[j]*partials[j];  //new lambda is old + stepsize * partial diff
+  }
+  for(int j = 0; j < n; ++j){
+    Gt(copula(j, 0), copula(j, 1)) += pow(partials(copula(j, 0), copula(j, 1)),2); //Each GT is the sum of the squared partial diffs. 
+    pt(copula(j, 0), copula(j, 1)) = tuning * pow(Gt(copula(j, 0), copula(j, 1)), -0.5); //transform to get pt value
+    par1(copula(j, 0), copula(j, 1)) = par1(copula(j, 0), copula(j, 1)) + 
+    pt(copula(j, 0), copula(j, 1)) * partials(copula(j, 0), copula(j, 1));  //new lambda is old + stepsize * partial diff
+  }
+  cube output(10, 10, 2);
+  output.slice(0) = par1;
+  output.slice(1) = Gt;
+  return output;
+}
+
+// [[Rcpp::export]]
+cube SGA_eta2(vec y, vec theta, vec lambda, mat par1, mat par2, double tuning, mat Gt, mat copula, mat type, int s){
+  int n = copula.n_rows;
+  mat partials(10, 10, fill::zeros); //Store partial derivatives in calculation 
+  mat pt(10, 10, fill::zeros); //Step size
+  for(int i = 0; i < s; i++){ //for each sample in s
+    for(int j = 0; j < n; ++j){ //and for each parameter in eta
+      partials(copula(j, 0), copula(j, 1)) += 
+        deriv_eta2(theta.row(i), lambda, par1, par2, y, copula, type, j)/s;
+      //partial deriv = mean of s monte carlo estimates
     }
-    LBnew = ELBOc(lnew, y); //calculate lowerbound with new variables
-    diff = abs(LBnew - LBold); //absolute value of the difference
-    k = k + 1; //count iterations so far
-  } 
-  //Print k and LBnew (or add to return vector)
-  return lnew;
+  }
+  for(int j = 0; j < n; ++j){
+    Gt(copula(j, 0), copula(j, 1)) += pow(partials(copula(j, 0), copula(j, 1)),2); //Each GT is the sum of the squared partial diffs. 
+    pt(copula(j, 0), copula(j, 1)) = tuning * pow(Gt(copula(j, 0), copula(j, 1)), -0.5); //transform to get pt value
+    par2(copula(j, 0), copula(j, 1)) = par2(copula(j, 0), copula(j, 1)) + 
+      pt(copula(j, 0), copula(j, 1)) * partials(copula(j, 0), copula(j, 1));  //new lambda is old + stepsize * partial diff
+  }
+  cube output(10, 10, 2);
+  output.slice(0) = par1;
+  output.slice(1) = Gt;
+  return output;
 }
