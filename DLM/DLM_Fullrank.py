1635a3dde9059d076fdc8808d89a7d8babd4f1bb
NTomasetti <nat.tomasetti@gmail.com>
2017-03-20 15:56:30 +1100

    python changes

--- DLM/DLM_Fullrank.py
+++ DLM/DLM_Fullrank.py
@@ -1,6 +1,6 @@
 #!/home/nltom2/.virtualenvs/Python/bin/python3
 import edward as ed
-from edward.models import Normal
+from edward.models import Normal, MultivariateNormalCholesky
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
@@ -39,35 +39,14 @@ for t in range(T):
     y[t] = Normal(mu=x[t]+b, sigma=tf.sqrt(yv))
 
 # Set up approximation
-qAr = Normal(mu = tf.Variable(tf.random_normal([])),
-            sigma=tf.nn.softplus(tf.Variable(tf.random_normal([]))))
-qB = Normal(mu = tf.Variable(tf.random_normal([])),
-            sigma=tf.nn.softplus(tf.Variable(tf.random_normal([]))))
-qX0 = Normal(mu = tf.Variable(tf.random_normal([])),
-            sigma=tf.nn.softplus(tf.Variable(tf.random_normal([]))))
-qXlv = Normal(mu = tf.Variable(tf.random_normal([])),
-            sigma=tf.nn.softplus(tf.Variable(tf.random_normal([]))))
-qYlv = Normal(mu = tf.Variable(tf.random_normal([])),
-            sigma=tf.nn.softplus(tf.Variable(tf.random_normal([]))))
-qX = [0] * (T)
-for t in range(T):
-    qX[t] = Normal(mu = tf.Variable(tf.random_normal([])),
-            sigma=tf.nn.softplus(tf.Variable(tf.random_normal([]))))
+qDist = MultivariateNormalCholesky(mu = tf.Variable(tf.random_normal([T+5])),
+                                   chol = tf.cholesky(tf.Variable(initial_value = np.identity(T+5), dtype=tf.float32)))
 
 # Inference
 parameters = {
-    ar: qAr,
-    b: qB,
-    x0: qX0,
-    xlv: qXlv,
-    ylv: qYlv
+    [ar, b, xlv, ylv, x]: qDist
 }
-states = {x: qX for x, qX in zip(x, qX)}
-parameters.update(states)
 data = {yt: yData for yt, yData in zip(y, yData)}
 inference = ed.KLqp(parameters, data)
 inference.run()
 
-samples = qAr.sample(100).eval()
-plt.hist(samples, bins = 20)
-plt.savefig("qAr.png")
--- DLM/SVM.py
+++ DLM/SVM.py
@@ -1,8 +1,8 @@
-#!/home/nltom2/.virtualenvs/Python/bin/python3
 import tensorflow as tf
 import edward as ed
 import numpy as np
-from edward.models import Normal, InverseGamma, Uniform
+from edward.models import Normal
+
 
 # Model Parameters
 T = 50
@@ -15,15 +15,18 @@ hTrue = [0] * T
 yTrue = [0] * T
 for t in range(T):
     if (t == 1):
-        hTrue[t] = np.random.normal(loc=gammaTrue / (1 - phiTrue), scale=sigmaTrue / np.sqrt(1 - phiTrue ** 2))
+        hTrue[t] = np.random.normal(loc=gammaTrue / (1 - phiTrue),
+                                   scale=sigmaTrue / np.sqrt(1 - phiTrue ** 2))
     else:
-        hTrue[t] = np.random.normal(loc=gammaTrue + phiTrue * hTrue[t - 1], scale=sigmaTrue)
+        hTrue[t] = np.random.normal(loc=gammaTrue + phiTrue * hTrue[t - 1],
+                                    scale=sigmaTrue)
     yTrue[t] = np.random.normal(loc=0, scale=np.exp(hTrue[t] / 2))
 
 # Prior distributions
 gamma = Normal(mu=0.0, sigma=5.0)
-phi = Uniform(-1.0, 1.0)
-sigma = InverseGamma(alpha=1.0, beta=1.0)
+phi = Normal(mu=0.0, sigma=0.5)
+lsigma = Normal(mu=0.0, sigma=1.0)
+sigma = tf.exp(lsigma)
 # Create h and y as lists
 h = [0] * T
 y = [0] * T
@@ -39,9 +42,8 @@ qGamma = Normal(mu=tf.Variable(tf.random_normal([])),
                 sigma=tf.nn.softplus(tf.Variable(tf.random_normal([]))))
 qPhi = Normal(mu=tf.Variable(tf.random_normal([])),
               sigma=tf.nn.softplus(tf.Variable(tf.random_normal([]))))
-qSigma = InverseGamma(
-    alpha=tf.nn.softplus(tf.Variable(tf.random_normal([]))),
-    beta=tf.nn.softplus(tf.Variable(tf.random_normal([]))))
+qlSigma = Normal(mu=tf.Variable(tf.random_normal([])),
+              sigma=tf.nn.softplus(tf.Variable(tf.random_normal([]))))
 qH = [0] * T
 for t in range(T):
     qH[t] = Normal(mu=tf.Variable(tf.random_normal([])),
@@ -51,10 +53,10 @@ for t in range(T):
 parameters = {
     gamma: qGamma,
     phi: qPhi,
-    sigma: qSigma,
+    lsigma: qlSigma,
 }
 states = {h: qH for h, qH in zip(h, qH)}
 parameters.update(states)
 data = {yt: yTrue for yt, yTrue in zip(y, yTrue)}
-inference = ed.KLqp(parameters, data)
+inference = ed.KLqp(parameters, data=data)
 inference.run()
